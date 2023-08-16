7587e6fac9af9961c558009516e9d2ff93c6385d
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-16 10:38:34 +0530

    Understand: Good practice

--- 01-intro/variableMe.ts
+++ 01-intro/variableMe.ts
@@ -1,6 +1,21 @@
 let greetings: string = "Hello World!";
 
-console.log(greetings)
+greetings.toLowerCase();
+console.log(greetings);
+
+// number (there is no integer or float in typescript)
+// let userId: number = 2334455;
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// Using type-inferences all the time is not the best use of .ts
+// It is actually overusing .ts and hence can be avoided
+// The code would obviously not throw an err
+
+let userId = 334455.3;   //Good enough to be used
+userId.toFixed();  
+
 
 // There is an error (to be handled later)
 // Handling it for the time being with the following command
