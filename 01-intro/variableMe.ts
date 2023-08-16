fd098ccf4eca7a69f16aabe4e4f95ab65da0f068
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-16 10:53:33 +0530

    Compelte: Variables

--- 01-intro/variableMe.ts
+++ 01-intro/variableMe.ts
@@ -16,6 +16,15 @@ let isLoggedIn: boolean = false;
 let userId = 334455.3;   //Good enough to be used
 userId.toFixed();  
 
+let hero: String;
+
+function getHero(){
+    return "thor";
+}
+hero = getHero();
+
+// People try to use "any"; which takes away the point
+// of using .ts in your project. And it can cause buggy code.
 
 // There is an error (to be handled later)
 // Handling it for the time being with the following command
