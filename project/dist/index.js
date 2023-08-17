ef70664c7a29463bf5d14c4f006074acc6c4e48c
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-17 22:08:00 +0530

    Using typescript in a project

---
+++ project/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("ts is not here");
+// You can run typescript compiler in watch mode
+// so that any changes that you save get compiled in
+// javasrit in the dist/ directory. This does not happen
+// when you manually compile as the destination directory
+// remains the same. 
