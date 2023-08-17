0d52705acb2c5b0ac16a9e4ca6526cc3921e4686
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-17 21:27:15 +0530

    Understanding enums

--- enums.js
+++ enums.js
@@ -1,22 +1,6 @@
 // Try compiling this into JavaScript
 // It generates a lot of extra code which is unnecessary
-
-// enum SeatChoice {
-//     AISLE = "aisle",
-//     MIDDLE = 3, 
-//     WINDOW, 
-//     FOURTH 
-// }
-
-// A solution to the previous problem is 
-const enum SeatChoice {
-    AISLE = "aisle",
-    MIDDLE = 3, 
-    WINDOW = "window",
-    FOURTH = "fourth" 
-}
-const hcSeat = SeatChoice.AISLE;
-
+var hcSeat = "aisle" /* SeatChoice.AISLE */;
 // While typescript can be abused to generate a lot of js code
 // You must try to write typescript code that actually 
 // generated elegant js code, so that in case someone has to 
