2cbfb00a6ef4de290a8d2edbcd39eb0a94f20902
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-17 22:22:04 +0530

    Project structure complete

--- project/dist/index.js
+++ project/dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
 console.log("ts is not here");
+console.log("ts is still awesome");
 // You can run typescript compiler in watch mode
 // so that any changes that you save get compiled in
 // javasrit in the dist/ directory. This does not happen
 // when you manually compile as the destination directory
 // remains the same. 
+// For running projects we have 3 options :
+// 1 - Live Server (VS Code Extension) - Runs on PORT:5000
+// 2 - Lite Server (npm package) - Runs on PORT: 3000
+// 3 - Nodemon (npm package -- a bit heavy)
