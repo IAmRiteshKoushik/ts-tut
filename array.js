73ba45cbe3c2703a2af8b91740109e12734d0fd0
Ritesh Koushik <riteshkoushik39@gmail.com>
2023-08-17 18:59:03 +0530

    Using arrays in .ts

--- array.js
+++ array.js
@@ -1,32 +1,19 @@
 // This is how you define arrays but the below method ensures that the arrays
 // is always empty. For defining an array that can withold contents
 // use the second definition of array. 
-const superHeros: [] = [] 
-const superHeroes: number[] = []
-const superVillians: string[] = []
-
+var superHeros = [];
+var superHeroes = [];
+var superVillians = [];
 // Alternate way of defining data
-const heroPower: Array<number> = []
-
-superVillians.push("spiderman")
-
-// Using types
-type User = {
-  name: string
-  isActive: boolean
-}
-
-const allUsers: User[] = []
-
+var heroPower = [];
+superVillians.push("spiderman");
+var allUsers = [];
 // This following does not work as the array accepts entries only in in the 
 // User datatype. Passing an empty string is not allowed. 
 // allUsers.push("");
-allUsers.push({name: "", isActive: true});
-
-
+allUsers.push({ name: "", isActive: true });
 // An example for ML models - Array of arrays
-const MLModels: number[][] = [
-  [255, 255, 255],
-  []
-]
-
+var MLModels = [
+    [255, 255, 255],
+    []
+];
